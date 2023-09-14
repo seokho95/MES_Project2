031665a761f2812a6df9e8a16b77d54b3b4fe60f
pjy <pjy4534@naver.com>
2023-09-14 16:09:20 +0900

    update obtainorder

--- src/main/java/com/project/mapper/ObtainOrderMapper.java
+++ src/main/java/com/project/mapper/ObtainOrderMapper.java
@@ -18,14 +18,46 @@ public interface ObtainOrderMapper {
 
 	List<ObtainDTO> SearchProduction(Map<String, Object>map);
 
-	int countAllOrderProduct();
+	//수주
+	int countAllObtainOrder();
 
-	int countAllOrderProduct1();
+	int countAllObtainOrder1();
 
-	int countAllOrderProduct2();
+	int countAllObtainOrder2();
+	
+	int countAllObtainOrder3();
+	
+	int countAllObtainOrder4();
+	
+	int countAllObtainOrder5();
 
+	List<ObtainDTO> SearchObtainByDate(Map<String, Object> map);
+	
+	List<ObtainDTO> SearchObtain(Map<String, Object> map);
+
+	//제품
 	int DeleteProduction(List<String> pNum);
+	
+	int countAllOrderProduct();
+	//pNum 중복확인
+	String checkpNum(String pNum);
 
-	//ObtainDTO ModiWriteProduction(String pNum);
+	int InsertProduction(ObtainDTO dto);
+
+	ObtainDTO ModiWriteProduction(String pNum);
+
+	int UpdateProduction(ObtainDTO dto);
+
+	int countAllOrderCompany();
+
+	List<ObtainDTO> SearchBusiness(Map<String, Object> map);
+
+	int InsertBusiness(ObtainDTO dto);
+
+	ObtainDTO ModiWriteBusiness(String cNum);
+
+	int UpdateBusiness(ObtainDTO dto);
+
+	int DeleteBusiness(List<String> cNum);
 
 }
--- src/main/java/com/project/service/ObtainOrderService.java
+++ src/main/java/com/project/service/ObtainOrderService.java
@@ -34,24 +34,96 @@ public class ObtainOrderService {
 		return obtainmapper.SearchProduction(map);
 	}
 
+	public int countAllObtainOrder() {
+		return obtainmapper.countAllObtainOrder();
+	}
+
+	public int countAllObtainOrder1() {
+		return obtainmapper.countAllObtainOrder1();
+	}
+	public int countAllObtainOrder2() {
+		return obtainmapper.countAllObtainOrder2();
+	}
+	public int countAllObtainOrder3() {
+		return obtainmapper.countAllObtainOrder3();
+	}
+	public int countAllObtainOrder4() {
+		return obtainmapper.countAllObtainOrder4();
+	}
+	public int countAllObtainOrder5() {
+		return obtainmapper.countAllObtainOrder5();
+	}
+
+	public int DeleteProduction(List<String> pNum) {
+		return obtainmapper.DeleteProduction(pNum);
+	}
+
+	public List<ObtainDTO> SearchObtainByDate(String startDate, String endDate, String startDate1, String endDate1, String kind, String search) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("startDate", startDate);
+		map.put("endDate", endDate);
+		map.put("startDate1", startDate1);
+		map.put("endDate1", endDate1);
+		map.put("kind", kind);
+		map.put("search", search);
+		return obtainmapper.SearchObtainByDate(map);
+	}
+
+	public List<ObtainDTO> SearchObtain(String kind, String search) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("kind", kind);
+		map.put("search", search);
+		return obtainmapper.SearchObtain(map);
+	}
+
 	public int countAllOrderProduct() {
 		return obtainmapper.countAllOrderProduct();
 	}
 
-	public int countAllOrderProduct1() {
-		return obtainmapper.countAllOrderProduct1();
-	}
-	public int countAllOrderProduct2() {
-		return obtainmapper.countAllOrderProduct2();
+	public boolean checkpNum(String pNum) {
+		String dto = obtainmapper.checkpNum(pNum);
+		return dto != null;
 	}
 
-	public int deleteProduction(List<String> pNum) {
-		return obtainmapper.DeleteProduction(pNum);
+	public int InsertProduction(ObtainDTO dto) {
+		return obtainmapper.InsertProduction(dto);
 	}
 
-//	public ObtainDTO ModiWriteProduction(String pNum) {
-//		return obtainmapper.ModiWriteProduction(pNum);
-//	}
+	public ObtainDTO ModiWriteProduction(String pNum) {
+		return obtainmapper.ModiWriteProduction(pNum);
+	}
+
+	public int UpdateProduction(ObtainDTO dto) {
+		return obtainmapper.UpdateProduction(dto);
+	}
+
+	//고객 카운트
+	public int countAllOrderCompany() {
+		return obtainmapper.countAllOrderCompany();
+	}
+
+	public List<ObtainDTO> SearchBusiness(String kind, String search) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("kind", kind);
+		map.put("search", search);
+		return obtainmapper.SearchBusiness(map);
+	}
+
+	public int InsertBusiness(ObtainDTO dto) {
+		return obtainmapper.InsertBusiness(dto);
+	}
+
+	public ObtainDTO ModiWriteBusiness(String cNum) {
+		return obtainmapper.ModiWriteBusiness(cNum);
+	}
+
+	public int UpdateBusiness(ObtainDTO dto) {
+		return obtainmapper.UpdateBusiness(dto);
+	}
+
+	public int DeleteBusiness(List<String> cNum) {
+		return obtainmapper.DeleteBusiness(cNum);
+	}
 	
 	
 }
